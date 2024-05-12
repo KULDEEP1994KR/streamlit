c4512178107873d94f2a150ed13f8871e7422e20
KULDEEP1994KR <123535141+KULDEEP1994KR@users.noreply.github.com>
2024-05-13 00:47:21 +0530

    Add files via upload

--- streamlit_new.py
+++ streamlit_new.py
@@ -2,8 +2,13 @@
 import streamlit as st
 import pickle
 import numpy as np
+from  langchain_openai import ChatOpenAI
+import os
+
 with open('model.pkl', 'rb') as f:
     model = pickle.load(f)
+
+
 def main():
    
     
@@ -29,6 +34,33 @@ def main():
     st.sidebar.markdown('<h1 style="font-family: sans-serif; color: white;">COMING SOON</h1>', unsafe_allow_html=True)
     predict1()
 
+
+
+os.environ["OPENAI_API_KEY"]= "sk-proj-0uY2W00u9BkZK2IIEVFVT3BlbkFJHC22MEogyR"
+llm= ChatOpenAI(temperature=0.7)
+
+
+# Display the text input with custom CSS styling
+
+
+st.markdown('<span style="font-family: Arial; font-size: 35px; color: white; font-weight: bold;">How may i help you:</span>', unsafe_allow_html=True)
+llm_string = st.text_input(" ")
+button_click=st.button("Ask GPT")
+
+
+if button_click:
+    if llm_string is not None:
+         result= llm.invoke(llm_string)
+
+         st.write("Results:")
+         st.write(result)
+        # st.info(result)
+    else:
+        st.warning("Enter some text to genrate results")
+
+
+
+
 # Add more Streamlit components here as needed
 def make_prediction(input_data):
     prediction = model.predict(np.array(input_data).reshape(-1, 1))
@@ -37,10 +69,11 @@ def make_prediction(input_data):
 
 def predict1():
     st.markdown('<h1 style="color: white;">Linear Regression Prediction</h1>', unsafe_allow_html=True)
-    st.write("Home Price Pridiction with area")
-    
     # Input features for prediction
-    feature = st.slider('Feature', min_value=0.0, max_value=1.0, step=0.01)
+    # feature = st.slider('Feature', min_value=0.0, max_value=1.0, step=0.01)
+    st.markdown('<span style="font-family: Arial; font-size: 25px;">Enter the area size to predict:</span>', unsafe_allow_html=True)
+
+    feature = st.number_input(" ", value=0, step=1)
     
     # Example of using the model to make predictions
     input_data = [feature]  # Assuming your model requires only one feature
